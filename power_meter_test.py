5e0f34a18d5532b2bd8257935d7e837349910ca7
Joe <jlian5@illinois.edu>
2020-12-08 15:23:07 -0600

    added r/t ratio in power meter test file

--- power_meter_test.py
+++ power_meter_test.py
@@ -4,11 +4,18 @@ import pyvisa as visa
 import time
 from ThorlabsPM100 import ThorlabsPM100
 rm = visa.ResourceManager()
-inst = rm.open_resource('USB::0x1313::0x8078::P0025003::INSTR', timeout=0)
+rightRes = rm.open_resource('USB::0x1313::0x8078::P0025003::INSTR', timeout=0)
+dnRes = rm.open_resource('USB::0x1313::0x8078::P0027639::INSTR', timeout=0)
 # print(rm.list_resources('?*'))
 # inst =rm.open_resource('USB0::0x0000::0x0000::DG5Axxxxxxxxx::INSTR', timeout=1)
-power_meter = ThorlabsPM100(inst=inst)
+p3 = ThorlabsPM100(inst=dnRes)
+p1 = ThorlabsPM100(inst=rightRes)
 
+sum: float = 0.0
+count: int = 0
 while 1:
-    # time.sleep(1)
-    print(power_meter.read)
+    count = count + 1
+    sum = sum + (p3.read / p1.read)
+    time.sleep(.3)
+    # print((pm_r.read) / pm_t.read)
+    print(sum / count)
